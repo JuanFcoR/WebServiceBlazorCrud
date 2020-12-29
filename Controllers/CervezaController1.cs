bf3443c3b27457642d46254afc450a181f6f996a
JuanFcoR <juan_rosa1@ucne.edu.do>
2020-12-29 00:28:49 -0400

    Add project files.

---
+++ Controllers/CervezaController1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebServiceBlazorCrud.Models;
+using WebServiceBlazorCrud.Models.Request;
+using WebServiceBlazorCrud.Models.Response;
+
+namespace WebServiceBlazorCrud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CervezaController1 : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Respuesta1 oRespuesta = new Respuesta1();
+            try
+            {
+
+                using (BlazorCrudContext db = new BlazorCrudContext())
+                {
+                    var lst = db.Cervezas.ToList();
+                    oRespuesta.Exito = 1;
+                    oRespuesta.Data = lst;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                oRespuesta.Mensaje = ex.Message;
+            }
+
+
+            return Ok(oRespuesta);
+        }
+        [HttpGet("{Id}")]
+        public IActionResult Get(int Id)
+        {
+            Cerveza cerveza = new Cerveza();
+            Respuesta1 oRespuesta = new Respuesta1();
+            try
+            {
+
+                using (BlazorCrudContext db = new BlazorCrudContext())
+                {
+                    cerveza = oRespuesta.Data.Where(c => c.Id == Id).FirstOrDefault();
+
+                    oRespuesta.Exito = 1;
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                oRespuesta.Mensaje = ex.Message;
+            }
+
+
+            return Ok(cerveza);
+        }
+
+        [HttpPost]
+        public IActionResult Add(CervezaRequest model)
+        {
+            Respuesta1 oRespuesta = new Respuesta1();
+            try
+            {
+
+                using (BlazorCrudContext db = new BlazorCrudContext())
+                {
+                    Cerveza oCerveza = new Cerveza();
+                    oCerveza.Nombre = model.Nombre;
+                    oCerveza.Marca = model.Marca;
+                    db.Cervezas.Add(oCerveza);
+                    db.SaveChanges();
+                    oRespuesta.Exito = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                // oRespuesta.Mensaje = ex.Message;
+                throw;
+            }
+
+
+            return Ok(oRespuesta);
+        }
+
+        [HttpPut]
+        public IActionResult Edit(CervezaRequest model)
+        {
+            Respuesta1 oRespuesta = new Respuesta1();
+            try
+            {
+
+                using (BlazorCrudContext db = new BlazorCrudContext())
+                {
+                    Cerveza oCerveza = db.Cervezas.Find(model.Id);
+                    oCerveza.Nombre = model.Nombre;
+                    oCerveza.Marca = model.Marca;
+                    db.Entry(oCerveza).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                    db.SaveChanges();
+                    oRespuesta.Exito = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                oRespuesta.Mensaje = ex.Message;
+            }
+
+
+            return Ok(oRespuesta);
+        }
+
+        [HttpDelete("{Id}")]
+        public IActionResult Delete(int Id)
+        {
+            Respuesta1 oRespuesta = new Respuesta1();
+            try
+            {
+
+                using (BlazorCrudContext db = new BlazorCrudContext())
+                {
+                    Cerveza oCerveza = db.Cervezas.Find(Id);
+                    db.Remove(oCerveza);
+                    db.SaveChanges();
+                    oRespuesta.Exito = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+
+            return Ok(oRespuesta);
+        }
+    }
+}
---
+++ Models/BlazorCrudContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace WebServiceBlazorCrud.Models
+{
+    public partial class BlazorCrudContext : DbContext
+    {
+        public BlazorCrudContext()
+        {
+        }
+
+        public BlazorCrudContext(DbContextOptions<BlazorCrudContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Cerveza> Cervezas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(
+                    "Server=tcp:juansfcorerver.database.windows.net,1433;Initial Catalog=BlazorCrud;Persist Security Info=False;User ID=juanfcor;Password=Softendo03;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+                   );
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Latin1_General_CI_AI");
+
+            modelBuilder.Entity<Cerveza>(entity =>
+            {
+                entity.ToTable("cerveza");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Marca)
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .HasColumnName("marca");
+
+                entity.Property(e => e.Nombre)
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .HasColumnName("nombre");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
